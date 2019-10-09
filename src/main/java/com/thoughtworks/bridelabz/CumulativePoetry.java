68969d3d971f1f513db38c893b51b1f8204d947a
chakravarthig <gchakravarthia8@gmail.com>
2019-10-09 12:49:35 +0530

    Add. it can duplicate the lines if You given cmd --echo

--- src/main/java/com/thoughtworks/bridelabz/CumulativePoetry.java
+++ src/main/java/com/thoughtworks/bridelabz/CumulativePoetry.java
@@ -1,21 +1,38 @@
 package com.thoughtworks.bridelabz;
 
 public class CumulativePoetry {
-    private String[] poem = {"the house that Jack built", "the malt that lay in\n", "the rat that ate\n", "the cat that killed\n","the dog that worried\n","that cow with the crumpled horn that tossed\n"};
+    private String[] poem = {"the house that Jack built\n",
+            "the malt that lay in\n",
+            "the rat that ate\n",
+            "the cat that killed\n",
+            "the dog that worried\n",
+            "that cow with the crumpled horn that tossed\n",
+            "the maiden all forlorn that milked\n",
+            "the man all tattered and torn that kissed\n",
+            "the priest all shaven and shorn that married\n",
+            "the rooster that crowed in the morn that woke\n",
+            "the farmer sowing his corn that kept\n",
+            "the horse and the hound and the horn that belonged to\n"};
 
     public static void main(String[] args) {
-        CumulativePoetry poet = new CumulativePoetry();
-        int day = Integer.parseInt(args[0]);
-        StringBuilder temp = new StringBuilder("This is ");
-        for (int i = day; i > 0; i--) {
-            temp.append(poet.poem[i - 1]);
+        String cmd = null;
+        int day;
+        if (args.length == 2) {
+            cmd = args[0];
+            day = Integer.parseInt(args[1]);
+        } else {
+            day = Integer.parseInt(args[0]);
         }
-        System.out.println(temp);
+        CumulativePoetry poet = new CumulativePoetry();
+        System.out.println(poet.poetry(day,cmd));
     }
 
-    public String poetry(int day) {
+    public String poetry(int day,String cmd) {
         StringBuilder temp = new StringBuilder("This is ");
         for (int i = day; i > 0; i--) {
+            if (cmd!=null&&cmd.equals("--echo")){
+                temp.append(poem[i - 1]);
+            }
             temp.append(poem[i - 1]);
         }
         return temp.toString();
--- src/test/java/com/thoughtworks/bridelabz/CumulativePoetryTest.java
+++ src/test/java/com/thoughtworks/bridelabz/CumulativePoetryTest.java
@@ -8,36 +8,69 @@ class CumulativePoetryTest {
     @Test
     void givenDayOne_WhenPoem_ThenShouldShowDayOnePoem() {
         CumulativePoetry poetry = new CumulativePoetry();
-        assertEquals("This is the house that Jack built", poetry.poetry(1));
+        assertEquals("This is the house that Jack built\n", poetry.poetry(1,null));
     }
 
     @Test
     void givenDayTwo_WhenPoem_ThenShouldShowDayTwoPoem() {
         CumulativePoetry poetry = new CumulativePoetry();
-        assertEquals("This is the malt that lay in\nthe house that Jack built", poetry.poetry(2));
+        assertEquals("This is the malt that lay in\nthe house that Jack built\n", poetry.poetry(2,null));
     }
 
     @Test
     void givenDayThree_WhenPoem_ThenShouldShowDayThreePoem() {
         CumulativePoetry poetry = new CumulativePoetry();
-        assertEquals("This is the rat that ate\nthe malt that lay in\nthe house that Jack built", poetry.poetry(3));
+        assertEquals("This is the rat that ate\nthe malt that lay in\nthe house that Jack built\n", poetry.poetry(3,null));
     }
 
     @Test
     void givenDayFour_WhenPoem_ThenShouldShowDayFourPoem() {
         CumulativePoetry poetry = new CumulativePoetry();
-        assertEquals("This is the cat that killed\nthe rat that ate\nthe malt that lay in\nthe house that Jack built", poetry.poetry(4));
+        assertEquals("This is the cat that killed\nthe rat that ate\nthe malt that lay in\nthe house that Jack built\n", poetry.poetry(4,null));
     }
+
     @Test
     void givenDayFive_WhenPoem_ThenShouldShowDayFivePoem() {
         CumulativePoetry poetry = new CumulativePoetry();
-        String poem="This is the dog that worried\nthe cat that killed\nthe rat that ate\nthe malt that lay in\nthe house that Jack built";
-        assertEquals(poem, poetry.poetry(5));
+        String poem = "This is the dog that worried\nthe cat that killed\nthe rat that ate\nthe malt that lay in\nthe house that Jack built\n";
+        assertEquals(poem, poetry.poetry(5,null));
     }
+
     @Test
     void givenDaySix_WhenPoem_ThenShouldShowDaySixPoem() {
         CumulativePoetry poetry = new CumulativePoetry();
-        String poem="This is that cow with the crumpled horn that tossed\nthe dog that worried\nthe cat that killed\nthe rat that ate\nthe malt that lay in\nthe house that Jack built";
-        assertEquals(poem, poetry.poetry(6));
+        String poem = "This is that cow with the crumpled horn that tossed\nthe dog that worried\nthe cat that killed\nthe rat that ate\nthe malt that lay in\nthe house that Jack built\n";
+        assertEquals(poem, poetry.poetry(6,null));
+    }
+
+    @Test
+    void givenDaySeven_WhenPoem_ThenShouldShowDaySevenPoem() {
+        CumulativePoetry poetry = new CumulativePoetry();
+        String poem = "This is the maiden all forlorn that milked\nthat cow with the crumpled horn that tossed\nthe dog that worried\nthe cat that killed\nthe rat that ate\nthe malt that lay in\nthe house that Jack built\n";
+        assertEquals(poem, poetry.poetry(7,null));
+    }
+    @Test
+    void givenDayTwelve_WhenPoem_ThenShouldShowDayTwelvePoem() {
+        CumulativePoetry poetry = new CumulativePoetry();
+        String poem = "This is the horse and the hound and the horn that belonged to\n" +
+                "the farmer sowing his corn that kept\n" +
+                "the rooster that crowed in the morn that woke\n" +
+                "the priest all shaven and shorn that married\n" +
+                "the man all tattered and torn that kissed\n" +
+                "the maiden all forlorn that milked\n" +
+                "that cow with the crumpled horn that tossed\n" +
+                "the dog that worried\n" +
+                "the cat that killed\n" +
+                "the rat that ate\n" +
+                "the malt that lay in\n" +
+                "the house that Jack built\n";
+        assertEquals(poem, poetry.poetry(12,null));
+    }
+
+    @Test
+    void givenDayOneAndEchoCmd_WhenPoem_ThenShouldShowDuplicateLines() {
+        CumulativePoetry poetry = new CumulativePoetry();
+        String  poem="This is the house that Jack built\nthe house that Jack built\n";
+        assertEquals(poem, poetry.poetry(1,"--echo"));
     }
 }
