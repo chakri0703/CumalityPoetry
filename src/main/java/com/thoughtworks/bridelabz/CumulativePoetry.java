979b4b2ed09478772b616aafed6463d571422cbf
chakravarthig <gchakravarthia8@gmail.com>
2019-10-09 10:41:59 +0530

    Add. Can Able to see Fifth Day Poem

--- src/main/java/com/thoughtworks/bridelabz/CumulativePoetry.java
+++ src/main/java/com/thoughtworks/bridelabz/CumulativePoetry.java
@@ -1,22 +1,23 @@
 package com.thoughtworks.bridelabz;
 
 public class CumulativePoetry {
-    String[]  poem={"the house that Jack built","the malt that lay in\n","the rat that ate\n","the cat that killed\n"};
+    private String[] poem = {"the house that Jack built", "the malt that lay in\n", "the rat that ate\n", "the cat that killed\n"," the dog that worried\n"};
 
     public static void main(String[] args) {
-        CumulativePoetry poet=new CumulativePoetry();
-        int day=Integer.parseInt(args[0]);
-        String temp="This is ";
-      for (int i=day;i>0;i--){
-          temp+=poet.poem[i-1];
-      }
+        CumulativePoetry poet = new CumulativePoetry();
+        int day = Integer.parseInt(args[0]);
+        StringBuilder temp = new StringBuilder("This is ");
+        for (int i = day; i > 0; i--) {
+            temp.append(poet.poem[i - 1]);
+        }
         System.out.println(temp);
     }
+
     public String poetry(int day) {
-        String temp="This is ";
-       for (int i=day;i>0;i--){
-           temp+=poem[i-1];
-       }
-       return temp;
+        StringBuilder temp = new StringBuilder("This is ");
+        for (int i = day; i > 0; i--) {
+            temp.append(poem[i - 1]);
+        }
+        return temp.toString();
     }
 }
--- src/test/java/com/thoughtworks/bridelabz/CumulativePoetryTest.java
+++ src/test/java/com/thoughtworks/bridelabz/CumulativePoetryTest.java
@@ -4,26 +4,34 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class CumulativePoetryTest {
+class CumulativePoetryTest {
     @Test
-    void givenDayOne_WhenPoem_ThenShouldShowDayOnePoem(){
-        CumulativePoetry poetry=new CumulativePoetry();
-        assertEquals("This is the house that Jack built",poetry.poetry(1));
+    void givenDayOne_WhenPoem_ThenShouldShowDayOnePoem() {
+        CumulativePoetry poetry = new CumulativePoetry();
+        assertEquals("This is the house that Jack built", poetry.poetry(1));
+    }
+
+    @Test
+    void givenDayTwo_WhenPoem_ThenShouldShowDayTwoPoem() {
+        CumulativePoetry poetry = new CumulativePoetry();
+        assertEquals("This is the malt that lay in\nthe house that Jack built", poetry.poetry(2));
+    }
+
+    @Test
+    void givenDayThree_WhenPoem_ThenShouldShowDayThreePoem() {
+        CumulativePoetry poetry = new CumulativePoetry();
+        assertEquals("This is the rat that ate\nthe malt that lay in\nthe house that Jack built", poetry.poetry(3));
+    }
+
+    @Test
+    void givenDayFour_WhenPoem_ThenShouldShowDayFourPoem() {
+        CumulativePoetry poetry = new CumulativePoetry();
+        assertEquals("This is the cat that killed\nthe rat that ate\nthe malt that lay in\nthe house that Jack built", poetry.poetry(4));
     }
     @Test
-    void givenDayTwo_WhenPoem_ThenShouldShowDayTwoPoem(){
-        CumulativePoetry poetry=new CumulativePoetry();
-        assertEquals("This is the malt that lay in\nthe house that Jack built",poetry.poetry(2));
-    }
-    @Test
-    void givenDayThree_WhenPoem_ThenShouldShowDayThreePoem(){
-        CumulativePoetry poetry=new CumulativePoetry();
-        assertEquals("This is the rat that ate\nthe malt that lay in\nthe house that Jack built",poetry.poetry(3));
-    }
-    @Test
-    void givenDayFour_WhenPoem_ThenShouldShowDayFourPoem(){
-        CumulativePoetry poetry=new CumulativePoetry();
-        assertEquals("This is the cat that killed\nthe rat that ate\nthe malt that lay in\nthe house that Jack built",poetry.poetry(4));
+    void givenDayFive_WhenPoem_ThenShouldShowDayFivePoem() {
+        CumulativePoetry poetry = new CumulativePoetry();
+        assertEquals("This is  the dog that worried\nthe cat that killed\nthe rat that ate\nthe malt that lay in\nthe house that Jack built", poetry.poetry(5));
     }
 
 }
